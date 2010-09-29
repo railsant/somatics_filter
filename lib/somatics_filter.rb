8ca1e66b210600e5d70213456817c647f163d366
Gary Or <garyor@Inspire-Synergys-MacBook.local>
2010-09-29 18:27:49 +0800

    Added Rails3 Support

--- lib/somatics_filter.rb
+++ lib/somatics_filter.rb
@@ -5,3 +5,6 @@ ActiveRecord::Base.send :include, SomaticsFilter::ActiveRecord::CoreExt
 ActionController::Base.send :helper, SomaticsFilter::Helpers::SomaticsFilterHelper
 
 I18n.load_path += Dir[File.expand_path(File.dirname(__FILE__) + '/../config/locales/*.yml')]
+
+# Use MetaSearch for Rails 3
+SomaticsFilter::Query.adapter = :meta_search if Rails.versions.starts_with('3')
