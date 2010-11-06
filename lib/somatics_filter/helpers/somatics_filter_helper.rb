f3df804e7b6ca92bba1c4deb8c8e76ebf304d395
PingLamb <pinglamb@pinglamb-VBHome.(none)>
2010-11-06 15:59:22 +0800

    somatics_filter_for accetps symbol rather than the model name

--- lib/somatics_filter/helpers/somatics_filter_helper.rb
+++ lib/somatics_filter/helpers/somatics_filter_helper.rb
@@ -1,7 +1,8 @@
 module SomaticsFilter
   module Helpers
     module SomaticsFilterHelper
-      def somatics_filter_for(model, options = {})
+      def somatics_filter_for(model_name, options = {})
+        model = model_name.to_s.camelize.constantize
         file = SomaticsFilter::Util.rails3? ? 'filter' : 'filter_rails2'
         render :file => "#{File.dirname(__FILE__)}/_#{file}.html.erb", :locals => {:somatics_filter_query => model.somatics_filter_query}
       end
